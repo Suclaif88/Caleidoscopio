34facb7e0b50e32e65895840c53222dd316fa4c4
Pandaazulmarino <146028502+Pandaazulmarino@users.noreply.github.com>
2024-03-20 16:29:48 -0500

    final

---
+++ PHP/AGGMA.php
@@ -0,0 +1,27 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    require_once "CONN.php";
+
+    if (!$conexion) {
+        die("La conexión a la base de datos falló: " . mysqli_connect_error());
+    }
+
+    $material = mysqli_real_escape_string($conexion, $_POST['material']);
+    $unidad = mysqli_real_escape_string($conexion, $_POST['unidad']);
+    $descripcion = mysqli_real_escape_string($conexion, $_POST['descripcion']);
+
+    $insertar = "INSERT INTO agregar_materiales (material, unidad, descripcion) VALUES ('$material', '$unidad', '$descripcion')";
+
+    if (mysqli_query($conexion, $insertar)) {
+        echo '<script>alert("Material agregado correctamente.");window.location.href = "../agr_material.html";</script>';
+    } else {
+        echo "Error al agregar el material: " . mysqli_error($conexion);
+    }
+
+    mysqli_close($conexion);
+} else {
+    echo "No se recibieron datos del formulario.";
+}
+?>

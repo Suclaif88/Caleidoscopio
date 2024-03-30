4289e1c1e10c4139542ab3b7f4b64f69589d6a70
Pandaazulmarino <146028502+Pandaazulmarino@users.noreply.github.com>
2024-03-29 22:18:06 -0500

    organizar formulario ur

--- VISTARE/FURG.php
+++ VISTARE/FURG.php
@@ -121,9 +121,9 @@
             ?>
                 </select>
                 <label for="cantidad">Cantidad:</label>
-                <input type="number" name="cantidades[]" required>
+                <input type="number" name="cantidad[]" required>
                 <label for="unidad">Unidad:</label>
-                <input type="text" name="unidades[]" required>
+                <input type="text" name="unidad[]" required>
             </div>
             <button style="background-color: #d6941a; border-radius: 15px; cursor:pointer;" type="button" onclick="eliminarProducto(this)">Eliminar material</button>
         </div>
@@ -143,9 +143,9 @@
             ?>
                 </select>
                 <label for="cantidad">Cantidad:</label>
-                <input type="number" name="cantidades[]" required>
+                <input type="number" name="cantidad[]" required>
                 <label for="unidad">Unidad:</label>
-                <input type="text" name="unidades[]" required>
+                <input type="text" name="unidad[]" required>
             </div>
             <button style="background-color: #d6941a; border-radius: 15px; cursor:pointer;" type="button" onclick="eliminarProducto(this)">Eliminar material</button>
         </div>

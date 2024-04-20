d3933ca90a641088264a59a4354ede888aab960f
Mona Lisa <roldansebasdaza@gmail.com>
2024-04-20 17:07:51 -0500

    20/04/2024

--- PHP/LOGIN.php
+++ PHP/LOGIN.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 require_once('CONN.php');
 
@@ -11,42 +15,44 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $consulta = "SELECT nombre, rol, pass FROM usuarios WHERE id = :id";
     
-    $stmt = $conexion->prepare($consulta);
-    
-    $stmt->bindParam(':id', $ID);
-    
-    $stmt->execute();
+    try {
+        $stmt = $conexion->prepare($consulta);
+        $stmt->bindParam(':id', $ID);
+        $stmt->execute();
 
-    $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+        $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if ($resultado && password_verify($PASS, $resultado['pass'])) {
-        $USER_NAME = $resultado['nombre'];
-        $USER_ROL = $resultado['rol'];
+        if ($resultado && password_verify($PASS, $resultado['pass'])) {
+            $USER_NAME = $resultado['nombre'];
+            $USER_ROL = $resultado['rol'];
 
-        $_SESSION['nombre'] = $USER_NAME;
-        $_SESSION['rol'] = $USER_ROL;
+            $_SESSION['nombre'] = $USER_NAME;
+            $_SESSION['rol'] = $USER_ROL;
 
-        switch ($USER_ROL) {
-            case '1':
-                header('Location: ../ADMIN/ADMIN.php');
-                break;
-            case '2':
-                header('Location: ../VISTA/VISTA.php');
-                break;
-            case '3':
-                header('Location: ../VISTA/DE.php');
-                break;
-            case '4':
-                header('Location: ../VISTA/DO.php');
-                break;
-            case '5':
-                header('Location: ../VISTA/RE.php');
-                break;
-            default:
-                echo "Error en las credenciales de inicio de sesión.";
-                break;
+            switch ($USER_ROL) {
+                case '1':
+                    header('Location: ../ADMIN/ADMIN.php');
+                    break;
+                case '2':
+                    header('Location: ../VISTA/VISTA.php');
+                    break;
+                case '3':
+                    header('Location: ../VISTA/DE.php');
+                    break;
+                case '4':
+                    header('Location: ../VISTA/DO.php');
+                    break;
+                case '5':
+                    header('Location: ../VISTA/RE.php');
+                    break;
+                default:
+                    echo "Error en las credenciales de inicio de sesión.";
+                    break;
+            }
+        } else {
+            echo "Error en las credenciales de inicio de sesión.";
         }
-    } else {
-        echo "Error en las credenciales de inicio de sesión.";
+    } catch(PDOException $e) {
+        die("Error en la consulta: " . $e->getMessage());
     }
 }

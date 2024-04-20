7d56a7b2e2e272271e59f7adc4365e3a7270e86f
Mona Lisa <roldansebasdaza@gmail.com>
2024-04-20 16:35:21 -0500

    20/04/2024

--- PHP/CONN.php
+++ PHP/CONN.php
@@ -1,12 +1,17 @@
 <?php
 
-$servidor="localhost";
-$usuario="root";
-$clave="";
-$db="apes";
+try {
+    // Cadena de conexión URI para PostgreSQL
+    $dsn = "pgsql:host=ep-wandering-limit-a49gxudm.us-east-1.aws.neon.tech;port=5432;dbname=verceldb;user=default;password=IiuFUMp6T1Px;sslmode=require";
 
-$conexion=mysqli_connect($servidor,$usuario,$clave,$db);
+    // Crear una nueva instancia de PDO
+    $conexion = new PDO($dsn);
 
-if(!$conexion){
-    die("error de conexion: ".mysqli_connect_error());
+    // Establecer el modo de error de PDO a excepción
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Conexión exitosa";
+} catch(PDOException $e) {
+    // Manejo de errores
+    die("Error de conexión: " . $e->getMessage());
 }

0b05fac7b939fe0c399d97c7e3daf615545fb362
Mona Lisa <roldansebasdaza@gmail.com>
2024-04-22 23:07:50 -0500

    22/04/2024

---
+++ PHP/CONFIG.php
@@ -0,0 +1,6 @@
+<?php
+
+$servidor="localhost";
+$usuario="root";
+$clave="";
+$db="apes";
--- PHP/CONN.php
+++ PHP/CONN.php
@@ -1,12 +1,9 @@
 <?php
 
-$servidor="localhost";
-$usuario="root";
-$clave="";
-$db="apes";
+include 'CONFIG.php';
 
-$conexion=mysqli_connect($servidor,$usuario,$clave,$db);
+$conexion = mysqli_connect($servidor, $usuario, $clave, $db);
 
-if(!$conexion){
-    die("error de conexion: ".mysqli_connect_error());
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
 }

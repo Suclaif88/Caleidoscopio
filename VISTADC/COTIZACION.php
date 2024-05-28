04ebcb10648672b8ffa88cfaf68281893a7733f6
Mona Lisa <roldansebasdaza@gmail.com>
2024-05-28 11:54:38 -0500

    28/05/2024

--- VISTADC/COTIZACION.php
+++ VISTADC/COTIZACION.php
@@ -6,6 +6,7 @@ require_once("../PHP/REALIZAR_BUSQUEDA.php");
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cotizaciones</title>
     <link rel="stylesheet" href="../CSS/CSSCO.css">

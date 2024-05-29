5b1f4a647142d20cbca009a5c96b5607416e3926
Mona Lisa <roldansebasdaza@gmail.com>
2024-05-28 23:00:04 -0500

    28/05/2024

--- PHP/CONN.php
+++ PHP/CONN.php
@@ -7,3 +7,6 @@ $conexion = mysqli_connect($servidor, $usuario, $clave, $db);
 if (!$conexion) {
     die("Error de conexión: " . mysqli_connect_error());
 }
+
+// Establecer la codificación de caracteres a UTF-8
+mysqli_set_charset($conexion, "utf8");
